ab2b51f7ba1565fd5b9ff2a1f7fc703a0f2f50e7
jaamarks <jmarks@rti.org>
2024-07-03 10:56:52 -0400

    Make Typer app the main CLI

--- mapOA-API/python/mapOA-API.py
+++ mapOA-API/python/mapOA-API.py
@@ -1,40 +1,80 @@
-# -*- coding: utf-8 -*-
-"""
-Python script and commands to access mapOA (https://staging.iomics-mapoa.org/) data via the API
-
-The user will need to retrieve their API token from their mapOA user profile page
-The user will enter their personal API token in place of the text "API-TOKEN-HERE"
-
-The user will need to install the ckanapi (https://github.com/ckan/ckanapi) python module
-CKAN documentation of the API in a general context is available at https://github.com/ckan/ckanapi
-The following commands provide mapOA specific queries which an authorized user will be to perform.
-"""
-
-# Import the ckanapi module
+from typing import Optional
+import typer
 from ckanapi import RemoteCKAN
+import os
+from dotenv import load_dotenv
 
-# API call of the mapOA portal. Please make sure to retrieve your personal API token from your mapOA user profile page
-mapoa = RemoteCKAN('https://staging.iomics-mapoa.org/', apikey = 'API-TOKEN-HERE')
 
-# Use the 'package_search' action to retrieve a list (as a dictionary ['dict'] object) of all of the datasets you has access to. 
-# Note when 'include_private' option is 'True' all datasets (private and public) are displayed. While when 'include_private' option is 'False' only public datasets will be displayed.
-datasets = mapoa.call_action('package_search', {'include_private': True})
+app = typer.Typer()
 
-# Use the 'current_package_list_with_resources' to retrieve a list of all datasets, their metadata and resources included within it.
-packageResources = mapoa.call_action('current_package_list_with_resources')
+load_dotenv()  # This will load variables from the .env file
 
-# Use the 'tag_list' action to retrieve the list of tags associated with at least one dataset on mapOA
-tags = mapoa.call_action('tag_list')
+api_url = os.environ.get("MAPOA_API_URL")
+api_key = os.environ.get("MAPOA_API_TOKEN")
 
-# Use the 'package_show' action to retrieve all of the metadata associated with the listed dataset id (the id is the complete title of the dataset.
-# A generalized version (no specific dataset queried) and specific dataset example is shown below. 
-# For the generalized example, replace 'DATASET-ID-HERE' with the desired dataset ID.
-datasetMetadata_general = mapoa.call_action('package_show', {'id': 'DATASET-ID-HERE'})
-datasetMetadata = mapoa.call_action('package_show', {'id': 'me-cfs-and-ifs-case-control-rna-expression-study-on-monozygotic-twins-gse16059'})
+if not api_url:
+    raise ValueError("MAPOA_API_URL environment variable not set!")
+if not api_key:
+    raise ValueError("MAPOA_API_TOKEN environment variable not set!")
 
-# Use the 'package_search' action to conduct a dataset search of 'case-control'. 
-# The action will return a dictionary with each dataset as an element of the dictionary.
-# A generalized version (no specific query) and specific query is shown below.
-# For the generalized example, replace 'QUERY-HERE' with the desired query.
-searchExample_general = mapoa.call_action('package_search', {'q': 'QUERY-HERE'})
-searchExample = mapoa.call_action('package_search', {'q': 'case-control'})
+
+def get_mapoa_client():
+    """
+    Initializes and returns a RemoteCKAN client object with user credentials.
+    """
+    return RemoteCKAN(api_url, apikey=api_key)
+
+@app.command()
+def list_datasets(include_private: bool = True):
+    """
+    Lists all datasets (public or private based on the flag)
+    accessible to the user.
+    """
+    mapoa = get_mapoa_client()
+    datasets = mapoa.call_action("package_search", {"include_private": include_private})
+    typer.echo(f"Datasets: {datasets}")
+
+
+@app.command()
+def list_datasets_with_resources():
+    """
+    Lists all datasets along with their metadata and resources.
+    """
+    mapoa = get_mapoa_client()
+    package_resources = mapoa.call_action("current_package_list_with_resources")
+    typer.echo(f"Package Resources: {package_resources}")
+
+
+@app.command()
+def list_tags():
+    """
+    Lists all tags associated with at least one dataset on mapOA.
+    """
+    mapoa = get_mapoa_client()
+    tags = mapoa.call_action("tag_list")
+    typer.echo(f"Tags: {tags}")
+
+
+@app.command()
+def show_dataset(dataset_id: str):
+    """
+    Shows detailed metadata for a specific dataset identified by its ID.
+    """
+    mapoa = get_mapoa_client()
+    dataset_metadata = mapoa.call_action("package_show", {"id": dataset_id})
+    typer.echo(f"Dataset Metadata: {dataset_metadata}")
+
+
+@app.command()
+def search_datasets(query: str):
+    """
+    Searches for datasets based on the provided query string.
+    """
+    mapoa = get_mapoa_client()
+    search_results = mapoa.call_action("package_search", {"q": query})
+    typer.echo(f"Search Results: {search_results}")
+
+
+if __name__ == "__main__":
+    app()
+
